983f1404cfc9818018c93d859fbc93122e22a4e9
egorzainullin <egor.zainullin@outlook.com>
2017-09-04 22:23:26 +0300

    added method add and print

---
+++ TreeWithIterator/TreeWithIteratorTests/TreeWithIteratorTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TreeWithIteratorTests
+{
+    [TestClass]
+    public class TreeWithIteratorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
