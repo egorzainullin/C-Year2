36ef6a530a161f37c8f52558bf18bd3ad98b3d8d
egorzainullin <egor.zainullin@outlook.com>
2017-09-07 22:43:42 +0300

    Added new tree's methods

--- TreeWithIterator/TreeWithIterator/Program.cs
+++ TreeWithIterator/TreeWithIterator/Program.cs
@@ -8,9 +8,19 @@ namespace TreeWithIterator
         {
             Console.WriteLine("Hello World!");
             var tree = new TreeWithIterator<int>();
-            tree.Add(1);
             tree.Add(2);
+            tree.Add(3);
+            tree.Add(1);
+            tree.Add(4);
+            tree.Add(5);
             tree.Print();
+            tree.Remove(1);
+            tree.Remove(2);
+            tree.Print();
+            foreach(var i in tree)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
--- TreeWithIterator/TreeWithIterator/TreeWithIterator.cs
+++ TreeWithIterator/TreeWithIterator/TreeWithIterator.cs
@@ -22,53 +22,183 @@ namespace TreeWithIterator
                 Print(root.LeftChild);
                 Print(root.RightChild);
             }
+            Console.WriteLine("===");
         }
 
         public void Add(T value)
         {
             Add(ref root, value);
-          
         }
 
-          void Add(ref TreeElement root, T value)
+        private void Add(ref TreeElement root, T value)
+        {
+            if (root == null)
             {
-               if (root == null)
-               {
-                   root = new TreeElement(null, null, value);
-                   return;
-               }
-            //    if (root.LeftChild == null && value.CompareTo(root.Value) == -1)
-            //    {
-            //        var newElement = new TreeElement(null, null, value);
-            //        root.LeftChild = newElement;
-            //    }
-            //    if (root.RightChild == null && value.CompareTo(root.Value) == 1)
-            //    {
-            //        var newElement = new TreeElement(null, null, value);
-            //        root.RightChild = newElement;
-            //    }
-               switch(value.CompareTo(root.Value))
-               {
-                   case 0: 
-                   return;
-                   case 1:
-                   Add(ref root.RightChild, value);
-                   break;
-                   case -1:
-                   Add(ref root.LeftChild, value);
-                   break;
-               }
+                root = new TreeElement(null, null, value);
+                return;
+            }
+            switch (value.CompareTo(root.Value))
+            {
+                case 0:
+                    return;
+                case 1:
+                    Add(ref root.RightChild, value);
+                    break;
+                case -1:
+                    Add(ref root.LeftChild, value);
+                    break;
+            }
+        }
+
+        private TreeElement SearchFor(T value)
+        {
+            return SearchFor(root);
+            TreeElement SearchFor(TreeElement root)
+            {
+                if (root == null)
+                {
+                    return null;
+                }
+                switch (value.CompareTo(root.Value))
+                {
+                    case 0:
+                        return root;
+                    case 1:
+                        return SearchFor(root.RightChild);
+                    case -1:
+                        return SearchFor(root.LeftChild);
+                    default:
+                        return null;
+                }
+            }
+        }
+
+        public bool IsContained(T value) => SearchFor(value) != null;
+
+        /// <summary>
+        /// Removes value from collection
+        /// </summary>
+        /// <param name="value">Value to remove</param>
+        public void Remove(T value)
+        {
+            var valueToRemove = SearchFor(value);
+            if (valueToRemove == null)
+            {
+                return;
+            }
+            DeleteNode(ref this.root, value);
+
+        }
+
+        private void DeleteNode(ref TreeElement node, T value)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            switch (value.CompareTo(node.Value))
+            {
+                case -1:
+                    DeleteNode(ref node.LeftChild, value);
+                    break;
+                case 1:
+                    DeleteNode(ref node.RightChild, value);
+                    break;
+                case 0:
+                    if (node.LeftChild == null)
+                    {
+                        node = node.RightChild;
+                        return;
+                    }
+                    if (node.RightChild == null)
+                    {
+                        node = node.RightChild;
+                        return;
+                    }
+                    var nearestNode = node.LeftChild;
+                    nearestNode = FindNearest(node, value);
+                    node.Value = nearestNode.Value;
+                    DeleteNode(ref node.LeftChild, nearestNode.Value);
+                    return;
+            }
+        }
+
+        private TreeElement FindNearest(TreeElement node, T value)
+        {
+            var nearestNode = node;
+            switch (value.CompareTo(root.Value))
+            {
+                case 0:
+                    return root;
+                case 1:
+                    return FindNearest(root.RightChild, value);
+                case -1:
+                    return FindNearest(root.LeftChild, value);
+                default:
+                    return null;
+            }
+        }
+
+        private class TreeEnumerator : IEnumerator<T>
+        {
+            private Stack<TreeElement> stack;
+
+            private TreeElement root;
+
+            private bool IsJustStarted;
+
+            public TreeEnumerator(TreeElement root)
+            {
+                stack = new Stack<TreeElement>();
+                this.root = root;
+                stack.Push(root);
+                IsJustStarted = true;
             }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            public T Current => stack.Peek().Value;
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+                this.stack = null;
+                this.root = null;
+            }
+
+            public bool MoveNext()
+            {
+                if (IsJustStarted)
+                {
+                    IsJustStarted = false;
+                    return true;
+                }
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+                var poppedNode = stack.Pop();
+                if (poppedNode.LeftChild != null)
+                {
+                    stack.Push(poppedNode.LeftChild);
+                }
+                if (poppedNode.RightChild != null)
+                {
+                    stack.Push(poppedNode.RightChild);
+                }
+                return stack.Count != 0;
+            }
+
+            public void Reset()
+            {
+                stack = new Stack<TreeElement>();
+                stack.Push(root);
+                IsJustStarted = true;
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerator<T> GetEnumerator() => new TreeEnumerator(root);
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         private class TreeElement
         {
